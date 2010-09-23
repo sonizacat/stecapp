cda68b0a0aac6756997b84abaeee1d93016c1787
netardo <netardo@gmail.com>
2010-09-23 13:15:07 +0200

    Rails installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stecapp::Application.config.secret_token = 'af38a45ce7ad3296bf42f3009db5278488e550a18f4c6dd89b95987ec4b0d1e89fb3c1491036720c548aa8275e83bbce0ef621033b8aff11d6d7039d3f667956'
